2611fae83972b8f23aca75925126671203a59c77
Ollivier Robert <roberto@keltia.net>
2017-04-05 16:33:27 +0200

    Add APIEndpoint. Fill in Gondole variable.

--- gondole.go
+++ gondole.go
@@ -6,8 +6,14 @@ import (
 
 const (
     Version = "0.0"
+
+    APIEndpoint = "/api/v1"
 )
 
 func NewApp(name, redirectURI string) (g *Gondole, err error) {
+    g = &Gondole{
+        Name: name,
+        RedirectURI: redirectURI,
+    }
     return
 }
