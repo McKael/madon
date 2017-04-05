ba2a7e8031a7de2328ddfd1cd3424dc09e445444
Ollivier Robert <roberto@keltia.net>
2017-04-05 19:38:50 +0200

    Barebone CLI app.

--- cmd/gondole/main.go
+++ cmd/gondole/main.go
@@ -3,8 +3,37 @@ package main
 import (
     "log"
     "github.com/keltia/gondole"
+    "github.com/urfave/cli"
+    "os"
 )
 
+var (
+    fVerbose bool
+)
+
+func init() {
+}
+
 func main() {
-    log.Printf("Gondole CLI app v%s", gondole.Version)
+    cli.VersionFlag = cli.BoolFlag{Name: "version, V"}
+
+    cli.VersionPrinter = func(c *cli.Context) {
+        log.Printf("API wrapper: %s Mastodon CLI: %s\n", c.App.Version, gondole.Version)
+    }
+
+    app := cli.NewApp()
+    app.Name = "gondole"
+    app.Usage = "Mastodon CLI interface"
+    app.Author = "Ollivier Robert <roberto@keltia.net>"
+    app.Version = gondole.Version
+    //app.HideVersion = true
+
+    app.Flags = []cli.Flag{
+        cli.BoolFlag{
+            Name:        "verbose,v",
+            Usage:       "verbose mode",
+            Destination: &fVerbose,
+        },
+    }
+    app.Run(os.Args)
 }
