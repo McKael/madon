b40cc293e8dcb4ede0f6f2d51ba5ab2c7e1922f1
Ollivier Robert <roberto@keltia.net>
2017-04-05 16:35:04 +0200

    Begin to fill in types.

--- types.go
+++ types.go
@@ -1,18 +1,41 @@
 package gondole
 
-type Gondole struct {
+import "time"
 
+type Gondole struct {
+	Name string
+    RedirectURI string
 }
 
 type Account struct {
-
+	ID         int
+	Acct       string
+	Avatar     string
+	Followers  int
+	Followings int
+	Header     string
+	Note       string
+	Statuses   int
+	URL        string
+	Username   string
 }
 
 type Client struct {
-
+	BaseURL     string
+	BearerToken string
 }
 
 type Status struct {
-
+	ID         int
+	Account    *Account
+	Content    string
+	CreatedAT  time.Time
+	Favourited bool
+	Favourites int
+	InReplyTo  int
+	Reblog     *Status
+	Reblogged  bool
+	Reblogs    int
+	URI        string
+	URL        string
 }
-
