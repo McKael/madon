430cf72af4331bb4ede8e5328e8af5ba11d4df01
Ollivier Robert <roberto@keltia.net>
2017-04-05 16:34:22 +0200

    Test members.

--- gondole_test.go
+++ gondole_test.go
@@ -10,4 +10,7 @@ func TestNewApp(t *testing.T) {
     g, err := NewApp("foo", "bar")
     assert.NoError(t, err, "no error")
     assert.Equal(t, reflect.TypeOf(&Gondole{}), reflect.TypeOf(g), "should be Gondole")
+
+    assert.Equal(t, "foo", g.Name, "should be equal")
+    assert.Equal(t, "bar", g.RedirectURI, "should be equal")
 }
