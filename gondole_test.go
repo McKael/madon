cb57403cb08caf49bccba6823920e4c8a4f4dbab
Ollivier Robert <roberto@keltia.net>
2017-04-05 16:34:49 +0200

    gofmt.

--- gondole_test.go
+++ gondole_test.go
@@ -1,16 +1,16 @@
 package gondole
 
 import (
-    "reflect"
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
 )
 
 func TestNewApp(t *testing.T) {
-    g, err := NewApp("foo", "bar")
-    assert.NoError(t, err, "no error")
-    assert.Equal(t, reflect.TypeOf(&Gondole{}), reflect.TypeOf(g), "should be Gondole")
+	g, err := NewApp("foo", "bar")
+	assert.NoError(t, err, "no error")
+	assert.Equal(t, reflect.TypeOf(&Gondole{}), reflect.TypeOf(g), "should be Gondole")
 
-    assert.Equal(t, "foo", g.Name, "should be equal")
-    assert.Equal(t, "bar", g.RedirectURI, "should be equal")
+	assert.Equal(t, "foo", g.Name, "should be equal")
+	assert.Equal(t, "bar", g.RedirectURI, "should be equal")
 }
