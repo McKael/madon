c1ab517d8d2aab0c7f9fec54128a703eea6cd1a5
Ollivier Robert <roberto@keltia.net>
2017-04-10 21:40:05 +0200

    Rename the binary.

---
+++ cmd/gondole-cli/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/keltia/gondole"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+var (
+	fVerbose bool
+    instance *gondole.Gondole
+	cnf      *gondole.Config
+)
+
+func Register(c *cli.Context) (err error) {
+
+    instance, err = gondole.NewApp("gondole-cli", nil, gondole.NoRedirect)
+	return err
+}
+
+func init() {
+	cli.VersionFlag = cli.BoolFlag{Name: "version, V"}
+
+	cli.VersionPrinter = func(c *cli.Context) {
+		log.Printf("API wrapper: %s Mastodon CLI: %s\n", c.App.Version, gondole.APIVersion)
+	}
+}
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "gondole"
+	app.Usage = "Mastodon CLI interface"
+	app.Author = "Ollivier Robert <roberto@keltia.net>"
+	app.Version = gondole.APIVersion
+	//app.HideVersion = true
+
+	app.Before = Register
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:        "verbose,v",
+			Usage:       "verbose mode",
+			Destination: &fVerbose,
+		},
+	}
+	app.Run(os.Args)
+}
