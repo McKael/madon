084055129a8ee6857bc5b68d86a09f48fff18632
Ollivier Robert <roberto@keltia.net>
2017-04-10 19:36:10 +0200

    Add test for loadGlobal().

--- config_test.go
+++ config_test.go
@@ -1,2 +1,22 @@
 package gondole
 
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
+)
+
+func TestLoadGlobal(t *testing.T) {
+	baseDir = "."
+
+	_, err := loadGlobal(filepath.Join("test", "non.toml"))
+	assert.Error(t, err, "does not exist")
+
+	_, err = loadGlobal(filepath.Join("test", "perms.toml"))
+	assert.Error(t, err, "unreadable")
+
+	c, err := loadGlobal(filepath.Join("test", "config.toml"))
+	assert.NoError(t, err, "should read it fine")
+	assert.EqualValues(t, "foobar", c.Default, "equal")
+}
+
