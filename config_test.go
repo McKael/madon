ffbbd292c0500d28601388f889f7bad662f1ad50
Ollivier Robert <roberto@keltia.net>
2017-04-10 20:01:45 +0200

    Grrr, workaround the fact that one can not commit "000" files.

--- config_test.go
+++ config_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
+	"os"
 )
 
 func TestLoadGlobal(t *testing.T) {
@@ -12,8 +13,11 @@ func TestLoadGlobal(t *testing.T) {
 	_, err := loadGlobal(filepath.Join("test", "non.toml"))
 	assert.Error(t, err, "does not exist")
 
+	// git does now allow you to checkin 000 files :(
+	err = os.Chmod(filepath.Join("test", "perms.toml"), 000)
 	_, err = loadGlobal(filepath.Join("test", "perms.toml"))
 	assert.Error(t, err, "unreadable")
+	err = os.Chmod(filepath.Join("test", "perms.toml"), 600)
 
 	c, err := loadGlobal(filepath.Join("test", "config.toml"))
 	assert.NoError(t, err, "should read it fine")
