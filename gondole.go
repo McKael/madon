149d0cc31d1e8114686dd3d1482c056cc7528df2
Ollivier Robert <roberto@keltia.net>
2017-04-10 16:20:38 +0200

    The API endpoint does not need the trailing "/".

--- gondole.go
+++ gondole.go
@@ -20,7 +20,7 @@ var (
 
 // prepareRequest insert all pre-defined stuff
 func (g *Gondole) prepareRequest(what string) (req rest.Request) {
-	endPoint := APIEndpoint + fmt.Sprintf("/%s/", what)
+	endPoint := APIEndpoint + fmt.Sprintf("/%s", what)
 
 	// Add at least one option, the APIkey if present
 	hdrs := make(map[string]string)
