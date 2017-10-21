2af0b82ba1fb25f4ec7018883e2eae7ab0e9f4c2
Mikael Berthe <mikael@lilotux.net>
2017-10-21 18:52:38 +0200

    Change version to 2.0.0-dev

--- madon.go
+++ madon.go
@@ -23,7 +23,7 @@ type apiCallParams map[string]string
 
 const (
 	// MadonVersion contains the version of the Madon library
-	MadonVersion = "1.6.0"
+	MadonVersion = "2.0.0-dev"
 
 	// API version implemented in this library
 	apiVersion     = "v1"
