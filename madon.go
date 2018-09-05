1d58401d5da6ddc0e5931d7ef902285bb2f2e9c3
Mikael Berthe <mikael@lilotux.net>
2018-09-06 00:32:01 +0200

    Bump API version number

--- madon.go
+++ madon.go
@@ -23,7 +23,7 @@ type apiCallParams map[string]string
 
 const (
 	// MadonVersion contains the version of the Madon library
-	MadonVersion = "2.3.0"
+	MadonVersion = "2.4.0-dev"
 
 	// API version implemented in this library
 	apiVersion     = "v1"
