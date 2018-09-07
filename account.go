McKael/madon

commit 9dcadf72a1323935e376538b57ca14b91254acbc

Do not use a global API version
Mastodon uses an API version per endpoint (cf. search v2) so
we need to be able to specify the version for any API call.

account.go
@@ -62,7 +62,7 @@ func (mc *Client) updateRelationship(op string, id int64, params apiCallParams) 
 	}
 
 	var rel Relationship
-	if err := mc.apiCall(endPoint, method, params, nil, nil, &rel); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, method, params, nil, nil, &rel); err != nil {
 		return nil, err
 	}
 	return &rel, nil
@@ -92,7 +92,7 @@ func (mc *Client) getSingleAccount(op string, id int64) (*Account, error) {
 	}
 
 	var account Account
-	if err := mc.apiCall(endPoint, method, nil, nil, nil, &account); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, method, nil, nil, nil, &account); err != nil {
 		return nil, err
 	}
 	return &account, nil
@@ -104,7 +104,7 @@ func (mc *Client) getSingleAccount(op string, id int64) (*Account, error) {
 func (mc *Client) getMultipleAccounts(endPoint string, params apiCallParams, lopt *LimitParams) ([]Account, error) {
 	var accounts []Account
 	var links apiLinks
-	if err := mc.apiCall(endPoint, rest.Get, params, lopt, &links, &accounts); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, rest.Get, params, lopt, &links, &accounts); err != nil {
 		return nil, err
 	}
 	if lopt != nil { // Fetch more pages to reach our limit
@@ -112,7 +112,7 @@ func (mc *Client) getMultipleAccounts(endPoint string, params apiCallParams, lop
 		for (lopt.All || lopt.Limit > len(accounts)) && links.next != nil {
 			newlopt := links.next
 			links = apiLinks{}
-			if err := mc.apiCall(endPoint, rest.Get, params, newlopt, &links, &accountSlice); err != nil {
+			if err := mc.apiCall("v1/"+endPoint, rest.Get, params, newlopt, &links, &accountSlice); err != nil {
 				return nil, err
 			}
 			accounts = append(accounts, accountSlice...)
@@ -253,7 +253,7 @@ func (mc *Client) FollowRemoteAccount(uri string) (*Account, error) {
 	params["uri"] = uri
 
 	var account Account
-	if err := mc.apiCall("follows", rest.Post, params, nil, nil, &account); err != nil {
+	if err := mc.apiCall("v1/follows", rest.Post, params, nil, nil, &account); err != nil {
 		return nil, err
 	}
 	if account.ID == 0 {
@@ -367,7 +367,7 @@ func (mc *Client) GetAccountRelationships(accountIDs []int64) ([]Relationship, e
 	}
 
 	var rl []Relationship
-	if err := mc.apiCall("accounts/relationships", rest.Get, params, nil, nil, &rl); err != nil {
+	if err := mc.apiCall("v1/accounts/relationships", rest.Get, params, nil, nil, &rl); err != nil {
 		return nil, err
 	}
 	return rl, nil

domain.go
@@ -17,7 +17,7 @@ func (mc *Client) GetBlockedDomains(lopt *LimitParams) ([]DomainName, error) {
 	const endPoint = "domain_blocks"
 	var links apiLinks
 	var domains []DomainName
-	if err := mc.apiCall(endPoint, rest.Get, nil, lopt, &links, &domains); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, rest.Get, nil, lopt, &links, &domains); err != nil {
 		return nil, err
 	}
 	if lopt != nil { // Fetch more pages to reach our limit
@@ -25,7 +25,7 @@ func (mc *Client) GetBlockedDomains(lopt *LimitParams) ([]DomainName, error) {
 		for (lopt.All || lopt.Limit > len(domains)) && links.next != nil {
 			newlopt := links.next
 			links = apiLinks{}
-			if err := mc.apiCall(endPoint, rest.Get, nil, newlopt, &links, &domainSlice); err != nil {
+			if err := mc.apiCall("v1/"+endPoint, rest.Get, nil, newlopt, &links, &domainSlice); err != nil {
 				return nil, err
 			}
 			domains = append(domains, domainSlice...)
@@ -40,7 +40,7 @@ func (mc *Client) BlockDomain(domain DomainName) error {
 	const endPoint = "domain_blocks"
 	params := make(apiCallParams)
 	params["domain"] = string(domain)
-	return mc.apiCall(endPoint, rest.Post, params, nil, nil, nil)
+	return mc.apiCall("v1/"+endPoint, rest.Post, params, nil, nil, nil)
 }
 
 // UnblockDomain unblocks the specified domain
@@ -48,5 +48,5 @@ func (mc *Client) UnblockDomain(domain DomainName) error {
 	const endPoint = "domain_blocks"
 	params := make(apiCallParams)
 	params["domain"] = string(domain)
-	return mc.apiCall(endPoint, rest.Delete, params, nil, nil, nil)
+	return mc.apiCall("v1/"+endPoint, rest.Delete, params, nil, nil, nil)
 }

emoji.go
@@ -13,7 +13,7 @@ import (
 // GetCustomEmojis returns a list with the server custom emojis
 func (mc *Client) GetCustomEmojis(lopt *LimitParams) ([]Emoji, error) {
 	var emojiList []Emoji
-	if err := mc.apiCall("custom_emojis", rest.Get, nil, lopt, nil, &emojiList); err != nil {
+	if err := mc.apiCall("v1/custom_emojis", rest.Get, nil, lopt, nil, &emojiList); err != nil {
 		return nil, err
 	}
 	return emojiList, nil

endorsements.go
@@ -15,7 +15,7 @@ func (mc *Client) GetEndorsements(lopt *LimitParams) ([]Account, error) {
 	endPoint := "endorsements"
 	method := rest.Get
 	var accountList []Account
-	if err := mc.apiCall(endPoint, method, nil, lopt, nil, &accountList); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, method, nil, lopt, nil, &accountList); err != nil {
 		return nil, err
 	}
 	return accountList, nil

instance.go
@@ -18,7 +18,7 @@ import (
 // GetCurrentInstance returns current instance information
 func (mc *Client) GetCurrentInstance() (*Instance, error) {
 	var i Instance
-	if err := mc.apiCall("instance", rest.Get, nil, nil, nil, &i); err != nil {
+	if err := mc.apiCall("v1/instance", rest.Get, nil, nil, nil, &i); err != nil {
 		return nil, err
 	}
 	return &i, nil
@@ -29,16 +29,18 @@ func (mc *Client) GetCurrentInstance() (*Instance, error) {
 // resolved.
 func (mc *Client) GetInstancePeers() ([]InstancePeer, error) {
 	var peers []InstancePeer
-	if err := mc.apiCall("instance/peers", rest.Get, nil, nil, nil, &peers); err != nil {
+	if err := mc.apiCall("v1/instance/peers", rest.Get, nil, nil, nil, &peers); err != nil {
 		return nil, err
 	}
 	return peers, nil
 }
 
 // GetInstanceActivity returns current instance activity
+// The activity contains the counts of active users, locally posted statuses,
+// and new registrations in weekly buckets.
 func (mc *Client) GetInstanceActivity() ([]WeekActivity, error) {
 	var activity []WeekActivity
-	if err := mc.apiCall("instance/activity", rest.Get, nil, nil, nil, &activity); err != nil {
+	if err := mc.apiCall("v1/instance/activity", rest.Get, nil, nil, nil, &activity); err != nil {
 		return nil, err
 	}
 	return activity, nil

lists.go
@@ -21,7 +21,7 @@ func (mc *Client) GetList(listID int64) (*List, error) {
 	}
 	endPoint := "lists/" + strconv.FormatInt(listID, 10)
 	var list List
-	if err := mc.apiCall(endPoint, rest.Get, nil, nil, nil, &list); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, rest.Get, nil, nil, nil, &list); err != nil {
 		return nil, err
 	}
 	return &list, nil
@@ -40,7 +40,7 @@ func (mc *Client) GetLists(accountID int64, lopt *LimitParams) ([]List, error) {
 
 	var lists []List
 	var links apiLinks
-	if err := mc.apiCall(endPoint, rest.Get, nil, lopt, &links, &lists); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, rest.Get, nil, lopt, &links, &lists); err != nil {
 		return nil, err
 	}
 	if lopt != nil { // Fetch more pages to reach our limit
@@ -48,7 +48,7 @@ func (mc *Client) GetLists(accountID int64, lopt *LimitParams) ([]List, error) {
 		for (lopt.All || lopt.Limit > len(lists)) && links.next != nil {
 			newlopt := links.next
 			links = apiLinks{}
-			if err := mc.apiCall(endPoint, rest.Get, nil, newlopt, &links, &listSlice); err != nil {
+			if err := mc.apiCall("v1/"+endPoint, rest.Get, nil, newlopt, &links, &listSlice); err != nil {
 				return nil, err
 			}
 			lists = append(lists, listSlice...)
@@ -103,7 +103,7 @@ func (mc *Client) AddListAccounts(listID int64, accountIDs []int64) error {
 		qID := fmt.Sprintf("account_ids[%d]", i)
 		params[qID] = strconv.FormatInt(id, 10)
 	}
-	return mc.apiCall(endPoint, method, params, nil, nil, nil)
+	return mc.apiCall("v1/"+endPoint, method, params, nil, nil, nil)
 }
 
 // RemoveListAccounts removes the accounts from the given list
@@ -118,7 +118,7 @@ func (mc *Client) RemoveListAccounts(listID int64, accountIDs []int64) error {
 		qID := fmt.Sprintf("account_ids[%d]", i)
 		params[qID] = strconv.FormatInt(id, 10)
 	}
-	return mc.apiCall(endPoint, method, params, nil, nil, nil)
+	return mc.apiCall("v1/"+endPoint, method, params, nil, nil, nil)
 }
 
 func (mc *Client) setSingleList(method rest.Method, listID int64, params apiCallParams) (*List, error) {
@@ -129,7 +129,7 @@ func (mc *Client) setSingleList(method rest.Method, listID int64, params apiCall
 		endPoint = "lists"
 	}
 	var list List
-	if err := mc.apiCall(endPoint, method, params, nil, nil, &list); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, method, params, nil, nil, &list); err != nil {
 		return nil, err
 	}
 	return &list, nil

madon.go
@@ -25,9 +25,7 @@ const (
 	// MadonVersion contains the version of the Madon library
 	MadonVersion = "2.4.0-dev"
 
-	// API version implemented in this library
-	apiVersion     = "v1"
-	currentAPIPath = "/api/" + apiVersion
+	currentAPIPath = "/api"
 
 	// NoRedirect is the URI for no redirection in the App registration
 	NoRedirect = "urn:ietf:wg:oauth:2.0:oob"

media.go
@@ -101,7 +101,7 @@ func (mc *Client) UpdateMedia(mediaID int64, description, focus *string) (*Attac
 
 	endPoint := "media/" + strconv.FormatInt(mediaID, 10)
 	var attachment Attachment
-	if err := mc.apiCall(endPoint, rest.Put, params, nil, nil, &attachment); err != nil {
+	if err := mc.apiCall("v1/"+endPoint, rest.Put, params, nil, nil, &attachment); err != nil {
 		return nil, err
 	}
 	return &attachment, nil

commit 254e06a186e21b2c063291973dcb30cd584fe9f8

Fix endpoint for update_credentials (broken by 9dcadf72)

account.go
@@ -499,7 +499,7 @@ func (mc *Client) UpdateAccount(cmdParams UpdateAccountParams) (*Account, error)
 	w.Close()
 
 	// Prepare the request
-	req, err := mc.prepareRequest(endPoint, rest.Patch, params)
+	req, err := mc.prepareRequest("v1/"+endPoint, rest.Patch, params)
 	if err != nil {
 		return nil, errors.Wrap(err, "prepareRequest failed")
 	}